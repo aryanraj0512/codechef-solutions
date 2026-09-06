bb3bd004147d02cd5fd221815592743b301480ce
Aryan raj <2300033767@kluniversity.in>
2026-09-06 18:15:25 +0530

    Solved: SQLPBP09 (codechef)

--- codechef/Practice/SQLPBP09.sql
+++ codechef/Practice/SQLPBP09.sql
@@ -2,8 +2,8 @@
 // Platform: codechef
 // Language: SQL​
 // Verdict: Accepted
-// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP09?tab=statement
-// Solved on: 2026-09-05T16:29:23.684Z
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP09
+// Solved on: 2026-09-06T12:45:22.654Z
 
 SELECT department, COUNT(*) AS total_employees
 FROM Employees 
