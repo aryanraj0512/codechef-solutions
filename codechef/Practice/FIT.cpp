6d9c7ad080542080f54962e9de436acddb272e87
Aryan raj <2300033767@kluniversity.in>
2026-09-06 18:29:18 +0530

    Solved: FIT (codechef)

---
+++ codechef/Practice/FIT.cpp
@@ -0,0 +1,22 @@
+// Problem: FIT
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-math/BASICMATH/problems/FIT?tab=statement
+// Solved on: 2026-09-06T12:59:15.147Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int weeklyDistance = x * 2 * 5;
+        cout << weeklyDistance << endl;
+        
+    }
+    return 0;
+}
