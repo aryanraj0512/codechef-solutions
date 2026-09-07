55cfd2776a64004d8fcbba137debc9d7d84de2b0
Aryan raj <2300033767@kluniversity.in>
2026-09-08 00:12:40 +0530

    Solved: PROBLEM LINK: (codechef)

---
+++ codechef/Practice/DOUBLERENT.cpp
@@ -0,0 +1,17 @@
+// Problem: PROBLEM LINK:
+// Platform: codechef
+// Language: x = int(input())
+print(2*x)
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT?tab=solution
+// Solved on: 2026-09-07T18:42:39.849Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+	cout << 2 * X << endl;
+
+}
