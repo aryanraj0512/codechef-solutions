3269150ad384712a23df0b266fa4b648f1041eaf
Aryan raj <2300033767@kluniversity.in>
2026-09-08 00:04:51 +0530

    Solved: PROBLEM LINK: (codechef)

---
+++ codechef/Practice/CLEARDAY.cpp
@@ -0,0 +1,18 @@
+// Problem: PROBLEM LINK:
+// Platform: codechef
+// Language: x, y = map(int, input().split())
+print(7-x-y)
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY?tab=solution
+// Solved on: 2026-09-07T18:34:50.442Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X , Y;
+	cin >> X >> Y;
+	
+	cout << 7 - X - Y << endl;
+
+}
