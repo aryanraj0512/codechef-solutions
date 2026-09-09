84f3e62dc0237459a8fbf5f15a1c69a82516e008
Aryan raj <2300033767@kluniversity.in>
2026-09-09 20:08:17 +0530

    Solved: EGGBUY (codechef)

---
+++ codechef/Practice/EGGBUY.cpp
@@ -0,0 +1,8 @@
+// Problem: EGGBUY
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START255D/problems/EGGBUY
+// Solved on: 2026-09-09T14:38:14.987Z
+
+// source not captured automatically - open the solution page and copy it manually
