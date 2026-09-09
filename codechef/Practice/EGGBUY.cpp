b82ae10857dbb09f63cd0b1498e18e108fa5fc4a
Aryan raj <2300033767@kluniversity.in>
2026-09-09 20:11:26 +0530

    Solved: EGGBUY (codechef)

--- codechef/Practice/EGGBUY.cpp
+++ codechef/Practice/EGGBUY.cpp
@@ -3,6 +3,21 @@
 // Language: C++​
 // Verdict: Accepted
 // URL: https://www.codechef.com/START255D/problems/EGGBUY
-// Solved on: 2026-09-09T14:38:14.987Z
+// Solved on: 2026-09-09T14:41:23.148Z
 
-// source not captured automatically - open the solution page and copy it manually
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X, Y , F;
+	cin >> X >> Y >> F ;
+ 
+ int cost1 = X * 12;
+ int  cost2 = Y * 12 + F ;
+  
+  if( cost1 < cost2 ){
+      cout << cost1;
+  }else{
+      cout << cost2;
+  }
+}
