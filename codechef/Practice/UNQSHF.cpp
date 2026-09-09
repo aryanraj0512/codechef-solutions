b5d8c328a94c37e85f02137abd357c40e0361335
Aryan raj <2300033767@kluniversity.in>
2026-09-09 20:08:57 +0530

    Solved: UNQSHF (codechef)

---
+++ codechef/Practice/UNQSHF.cpp
@@ -0,0 +1,14 @@
+// Problem: UNQSHF
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START255D/problems/UNQSHF
+// Solved on: 2026-09-09T14:38:54.117Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+}
