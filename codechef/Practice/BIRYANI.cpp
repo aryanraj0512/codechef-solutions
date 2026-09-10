dd2d28a108c72699823efbbceff685bd366fdb96
Aryan raj <2300033767@kluniversity.in>
2026-09-10 23:04:14 +0530

    Solved: BIRYANI (codechef)

---
+++ codechef/Practice/BIRYANI.cpp
@@ -0,0 +1,21 @@
+// Problem: BIRYANI
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI?tab=statement
+// Solved on: 2026-09-10T17:34:12.263Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--){
+	int X , Y;
+	cin >> X >> Y;
+	
+	cout << X * Y << endl ;
+	
+}
+}
