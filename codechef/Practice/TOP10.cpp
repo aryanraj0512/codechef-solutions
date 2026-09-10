fae791d1cf218671b8da57364ba20867de11c5da
Aryan raj <2300033767@kluniversity.in>
2026-09-10 23:12:35 +0530

    Solved: TOP10 (codechef)

---
+++ codechef/Practice/TOP10.cpp
@@ -0,0 +1,24 @@
+// Problem: TOP10
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
+// Solved on: 2026-09-10T17:42:32.929Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T;
+cin >> T;
+while(T--){
+    int X;
+    cin >> X;
+    if(X <= 10){
+        cout << "YES" << endl;
+    }else {
+        cout << "No"<< endl;
+    }
+}
+
+}
