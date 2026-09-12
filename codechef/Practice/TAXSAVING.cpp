4bc1fc26bb2b9816558efa93120f88633b6d63dd
Aryan raj <2300033767@kluniversity.in>
2026-09-12 09:42:10 +0530

    Solved: TAXSAVING (codechef)

---
+++ codechef/Practice/TAXSAVING.cpp
@@ -0,0 +1,20 @@
+// Problem: TAXSAVING
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING
+// Solved on: 2026-09-12T04:12:09.132Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--){
+	    int X, Y;
+	    cin >> X >> Y;
+	    cout << X -Y << endl;
+	}
+
+}
