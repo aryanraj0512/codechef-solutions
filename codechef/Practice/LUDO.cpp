0fa338698733d9f17966c78ab627cce3cd371e3d
Aryan raj <2300033767@kluniversity.in>
2026-09-12 09:44:45 +0530

    Solved: LUDO (codechef)

---
+++ codechef/Practice/LUDO.cpp
@@ -0,0 +1,24 @@
+// Problem: LUDO
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUDO
+// Solved on: 2026-09-12T04:14:44.876Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int X;
+	    cin >> X;
+	    if( X == 6){
+	        cout << "Yes"<<endl;
+	    }else{
+	        cout << " No" << endl;
+	    }
+	}
+
+}
