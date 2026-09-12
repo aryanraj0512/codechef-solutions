58c7ef2b205876c93e3b2ecf1b5bb30371eca208
Aryan raj <2300033767@kluniversity.in>
2026-09-12 20:48:29 +0530

    Solved: PRACLIST (codechef)

---
+++ codechef/Practice/PRACLIST.cpp
@@ -0,0 +1,17 @@
+// Problem: PRACLIST
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+// Solved on: 2026-09-12T15:18:28.525Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int  X , Y;
+	cin >> X >> Y;
+	cout << X - Y << endl;
+	
+
+}
