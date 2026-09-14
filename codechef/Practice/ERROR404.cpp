bd376e59e4a4727a3510d238bc355c24e4838c5a
Aryan raj <2300033767@kluniversity.in>
2026-09-14 21:07:17 +0530

    Solved: ERROR404 (codechef)

---
+++ codechef/Practice/ERROR404.cpp
@@ -0,0 +1,22 @@
+// Problem: ERROR404
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+// Solved on: 2026-09-14T15:37:14.404Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+	
+	if(X == 404){
+	    cout << "NOT FOUND" << endl;
+	    
+	}else{
+	    cout << "FOUND" << endl;
+	}
+
+}
