fa1c165f8cdbf61a6f652d1dafaecdbef38f6189
Aryan raj <2300033767@kluniversity.in>
2026-09-14 21:16:14 +0530

    Solved: DONDRIVE (codechef)

---
+++ codechef/Practice/DONDRIVE.cpp
@@ -0,0 +1,20 @@
+// Problem: DONDRIVE
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DONDRIVE
+// Solved on: 2026-09-14T15:46:13.214Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--){
+        int N , X;
+        cin >> N >> X;
+        cout << N - X << endl; 
+    }
+
+}
