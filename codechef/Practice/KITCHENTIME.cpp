02b98e0ab9fa7c15fd6f799e5849e3c2724b6fb3
Aryan raj <2300033767@kluniversity.in>
2026-09-14 21:18:05 +0530

    Solved: KITCHENTIME (codechef)

---
+++ codechef/Practice/KITCHENTIME.cpp
@@ -0,0 +1,21 @@
+// Problem: KITCHENTIME
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENTIME
+// Solved on: 2026-09-14T15:48:03.603Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int X , Y;
+	    cin >> X >> Y;
+	    
+	    cout << - X + Y<< endl;
+	    }
+
+}
