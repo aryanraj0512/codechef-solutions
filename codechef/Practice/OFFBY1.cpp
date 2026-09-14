26e0cd4d93838954b5e80c6c3f849c779f718ced
Aryan raj <2300033767@kluniversity.in>
2026-09-14 21:14:01 +0530

    Solved: OFFBY1 (codechef)

---
+++ codechef/Practice/OFFBY1.cpp
@@ -0,0 +1,17 @@
+// Problem: OFFBY1
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1?tab=statement
+// Solved on: 2026-09-14T15:44:00.020Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A , B;
+	
+	cin >> A >> B;
+	
+    cout << (A+B) * 10 + 1;
+}
