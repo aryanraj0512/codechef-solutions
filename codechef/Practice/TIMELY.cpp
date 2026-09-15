76f7a980727369a6c3eef68a89908452e368e26e
Aryan raj <2300033767@kluniversity.in>
2026-09-15 22:24:41 +0530

    Solved: TIMELY (codechef)

---
+++ codechef/Practice/TIMELY.cpp
@@ -0,0 +1,25 @@
+// Problem: TIMELY
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TIMELY
+// Solved on: 2026-09-15T16:54:39.315Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--){
+	    int X;
+	    cin >> X;
+	    
+	    if(X <  30){
+	        cout << "No" << endl;
+	    }else{
+	        cout << " Yes" << endl;
+	    }
+	}
+
+}
