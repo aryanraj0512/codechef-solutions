57fc9cf80fe984398592e8d38dfe1dacac4017a0
Aryan raj <2300033767@kluniversity.in>
2026-09-17 21:36:44 +0530

    Solved: PUZHUNT (codechef)

---
+++ codechef/Practice/PUZHUNT.cpp
@@ -0,0 +1,20 @@
+// Problem: PUZHUNT
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+// Solved on: 2026-09-17T16:06:41.382Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	if(N >  6 && N <  8){
+	    cout << "Yes" << endl;
+	    
+	}else{
+	cout << "No" << endl;
+}
+}
