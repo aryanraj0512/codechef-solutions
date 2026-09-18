b7a910acfd337ca944409bacb6ada800d9622b83
Aryan raj <2300033767@kluniversity.in>
2026-09-18 13:43:28 +0530

    Solved: TALLER (codechef)

---
+++ codechef/Practice/TALLER.cpp
@@ -0,0 +1,26 @@
+// Problem: TALLER
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TALLER?tab=statement
+// Solved on: 2026-09-18T08:13:24.734Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int X , Y;
+	    cin>> X >> Y ;
+	    
+	    if(X > Y){
+	        cout << "A" << endl ;
+	        
+	    }else{
+	        cout << "B" << endl;  ;
+	    }
+	}
+
+}
