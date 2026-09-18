92a3b5701eee598bef46ecca3db2dc2231ade69f
Aryan raj <2300033767@kluniversity.in>
2026-09-18 13:38:49 +0530

    Solved: BNE_APT (codechef)

---
+++ codechef/Practice/BNE_APT.cpp
@@ -0,0 +1,25 @@
+// Problem: BNE_APT
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
+// Solved on: 2026-09-18T08:08:45.134Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N , M;
+    cin >> N >> M;
+    int X , Y;
+    cin >> X >> Y;
+    
+    N * X;
+    M * Y;
+    
+    int total = (N* X) + ( M * Y);
+    
+    cout << total << endl;
+    
+
+}
