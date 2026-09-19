16f012e6b4b8a982550134816413f5bb749a8e2b
Aryan raj <2300033767@kluniversity.in>
2026-09-19 22:30:19 +0530

    Solved: REACHTARGET (codechef)

---
+++ codechef/Practice/REACHTARGET.cpp
@@ -0,0 +1,21 @@
+// Problem: REACHTARGET
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACHTARGET
+// Solved on: 2026-09-19T17:00:18.574Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--){
+	    int X , Y;
+	     cin >> X >> Y;
+	     cout << X -Y << endl;
+	     
+	}
+
+}
