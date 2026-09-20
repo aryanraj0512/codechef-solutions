afec6adfd978cf9d2d1a48054e2be87fa1e93da3
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:22:41 +0530

    Solved: BESTOFTWO (codechef)

---
+++ codechef/Practice/BESTOFTWO.cpp
@@ -0,0 +1,24 @@
+// Problem: BESTOFTWO
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
+// Solved on: 2026-09-20T14:52:39.400Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int X , Y ;
+	    cin >> X >> Y;
+	    if(X > Y ){
+	        cout << X << endl;
+	    }else{
+	        cout << Y << endl;
+	    }
+	}
+
+}
