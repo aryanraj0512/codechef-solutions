59d27a3badf83d45622be1fe99223b22abeeba43
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:39:23 +0530

    Solved: CANDIVIDE (codechef)

---
+++ codechef/Practice/CANDIVIDE.cpp
@@ -0,0 +1,24 @@
+// Problem: CANDIVIDE
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CANDIVIDE
+// Solved on: 2026-09-20T15:09:21.359Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N ;
+	    if(N % 3 == 0){
+	        cout << " YES" << endl;
+	    }else{
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
