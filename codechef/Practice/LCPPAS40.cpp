e04fb418b6aeebd498141806dbdc107f6d4f1b64
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:52:04 +0530

    Solved: Print 6 divided by 2 (codechef)

---
+++ codechef/Practice/LCPPAS40.cpp
@@ -0,0 +1,24 @@
+// Problem: Print 6 divided by 2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LCPPAS1/problems/LCPPAS40
+// Solved on: 2026-09-20T15:22:02.530Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string one = "Coding";
+    string two = "on";
+    string three = "CodeChef";
+    
+    string full = one + " " + two + " " + three;
+    
+    cout << one << " - " << one.length() << endl;
+    cout << two << " - " << two.length() << endl;
+    cout << three << " - " << three.length() << endl;
+    cout << full << " - " << full.length() << endl;
+
+    return 0;
+}
