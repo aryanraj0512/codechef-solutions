418a6cc9516051597ddc866731df16919aa272f5
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:27:17 +0530

    Solved: PROBLEM LINK: (codechef)

---
+++ codechef/Practice/RIP2000.cpp
@@ -0,0 +1,18 @@
+// Problem: PROBLEM LINK:
+// Platform: codechef
+// Language: for _ in range(int(input())):
+    n = int(input())
+    print(4*n)
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000?tab=solution
+// Solved on: 2026-09-20T14:57:15.398Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	cout << N * (2000/500) << endl;
+
+}
