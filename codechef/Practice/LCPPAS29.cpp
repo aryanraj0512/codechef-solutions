40c3d127bebe17fff1341ab5b8ae177f36e7b3d8
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:48:15 +0530

    Solved: Print 6 divided by 2 (codechef)

---
+++ codechef/Practice/LCPPAS29.cpp
@@ -0,0 +1,18 @@
+// Problem: Print 6 divided by 2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LCPPAS1/problems/LCPPAS29
+// Solved on: 2026-09-20T15:18:13.320Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a , b;
+    a = 20;
+    b = 6;
+    cout << a/b;
+
+    return 0;
+}
