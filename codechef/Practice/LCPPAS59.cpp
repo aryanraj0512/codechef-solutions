aea469e27a93039029a79d538be0d9554ec57099
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:56:06 +0530

    Solved: Print 6 divided by 2 (codechef)

---
+++ codechef/Practice/LCPPAS59.cpp
@@ -0,0 +1,23 @@
+// Problem: Print 6 divided by 2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LCPPAS1/problems/LCPPAS59
+// Solved on: 2026-09-20T15:26:04.928Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num ;
+    if(num = 0){
+        cout << "Zero" << endl;
+        
+    }else if(num > 0){
+        cout << "Positive" << endl;
+    }else{
+        cout << "Negative" << endl;
+    }
+    return 0;
+}
