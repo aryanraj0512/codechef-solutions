aab1d357724a30f3d5eb320ea8ca6934919d0b9e
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:41:32 +0530

    Solved: CHEFONDATE (codechef)

---
+++ codechef/Practice/CHEFONDATE.cpp
@@ -0,0 +1,24 @@
+// Problem: CHEFONDATE
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHEFONDATE
+// Solved on: 2026-09-20T15:11:31.001Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int X,Y;
+	    cin >> X >> Y;
+	    if(X >= Y){
+	        cout << "Yes" << endl;
+	    }else{
+	        cout << "NO" << endl;
+	    }
+	}
+
+}
