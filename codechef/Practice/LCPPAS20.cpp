a377ec212955f503ac7d1af90c9018de5b0d74e6
Aryan raj <2300033767@kluniversity.in>
2026-09-20 20:46:26 +0530

    Solved: Print 6 divided by 2 (codechef)

---
+++ codechef/Practice/LCPPAS20.cpp
@@ -0,0 +1,16 @@
+// Problem: Print 6 divided by 2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LCPPAS1/problems/LCPPAS20
+// Solved on: 2026-09-20T15:16:24.146Z
+
+ #include <iostream>
+using namespace std;
+
+int main() {
+	for(int i=1;i<=5;i++){
+	    cout << i << "-" << i * i << endl;
+	}
+
+}
