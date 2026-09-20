369517db822bb41cdd97cad770f1ce74ce413f8b
Aryan raj <2300033767@kluniversity.in>
2026-09-20 21:25:40 +0530

    Solved: Print 6 divided by 2 (codechef)

---
+++ codechef/Practice/LCPPAS150.cpp
@@ -0,0 +1,26 @@
+// Problem: Print 6 divided by 2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/skill-test/LCPPAS1/problems/LCPPAS150
+// Solved on: 2026-09-20T15:55:38.510Z
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--){
+    int x ;
+    cin >> x ;
+    if(x % 2 == 0){
+        cout << "Even" << endl;
+    }else{
+        cout << "Odd" ;
+    }
+    }   
+    return 0;
+}
