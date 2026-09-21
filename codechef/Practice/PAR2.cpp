ec78dadf8bc77a12d465c0d97571a36cde4f8301
Aryan raj <2300033767@kluniversity.in>
2026-09-21 18:27:13 +0530

    Solved: PAR2 (codechef)

---
+++ codechef/Practice/PAR2.cpp
@@ -0,0 +1,24 @@
+// Problem: PAR2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+// Solved on: 2026-09-21T12:57:11.390Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N;
+	    if(N % 2 == 0){
+	        cout << "Yes" << endl;
+	    }else{
+	        cout << "No" << endl;
+	    }
+	}
+
+}
