40afdd8957d668848d617d65c552f61d8c7e29ab
Aryan raj <2300033767@kluniversity.in>
2026-09-22 22:16:16 +0530

    Solved: PRIZEPOOL (codechef)

---
+++ codechef/Practice/PRIZEPOOL.cpp
@@ -0,0 +1,21 @@
+// Problem: PRIZEPOOL
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRIZEPOOL
+// Solved on: 2026-09-22T16:46:13.937Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+	    int X , Y ;
+	    cin >> X >> Y;
+	    
+	     cout << (10 * X) + (90* Y) << endl ;
+	}
+
+}
