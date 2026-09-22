556e06c83b0ac4ccb6fe800aa17edd052a7b08ed
Aryan raj <2300033767@kluniversity.in>
2026-09-22 22:17:38 +0530

    Solved: CNTWRD (codechef)

---
+++ codechef/Practice/CNTWRD.cpp
@@ -0,0 +1,21 @@
+// Problem: CNTWRD
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+// Solved on: 2026-09-22T16:47:36.911Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ int T;
+ cin >> T;
+ while(T--){
+     int N , M;
+     cin >> N >> M;
+     
+     cout << N * M << endl;
+ }
+
+}
